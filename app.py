550127c64d6a32cd092bf6cf316119b8f72eeafb
Navera <naverazafar23@gmail.com>
2025-12-09 15:56:51 -0800

    added about page

--- app.py
+++ app.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "Hello World"
+
+@app.route('/about')
+def about():
+    return "About"
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
